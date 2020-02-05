bea77794f81badfb048ba35f5dcf08de9db3f764
RaquelMarques97 <araquelfmarques@gmail.com>
2020-02-05 21:32:33 +0000

    addcalendar

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Switch, Route } from "react-router-dom";
 import './App.css';
 import Navbar from './Navbar';
@@ -8,28 +8,33 @@ import Trip from './Trip';
 import TripKm from './TripKm';
 import Homepage from './Homepage';
 
+class App extends Component {
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <Switch>
-        <Route exact path='/'>
-          <Homepage />
-        </Route>
-        <Route path='/trip'>
-          <Trip />
-        </Route>
-        <Route path='/booking'>
-          <Booking />
-        </Route>
-        <Route path='/tripkm'>
-          <TripKm />
-        </Route>
-      </Switch>
-      <Footer />
-    </>
-  );
+  
+  
+  render() {
+    return (
+      <>
+        <Navbar />
+        <Switch>
+          <Route exact path='/'>
+            <Homepage />
+          </Route>
+          <Route path='/trip'>
+            <Trip />
+          </Route>
+          <Route path='/booking'>
+            <Booking />
+          </Route>
+          <Route path='/tripkm'>
+            <TripKm />
+          </Route>
+        </Switch>       
+        
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
--- front/src/Booking.js
+++ front/src/Booking.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import Agenda from './Agenda';
 
 const Booking = () => {
-    return(
+    return (
         <div className='booking-container'>
-            <h1>title</h1>
+            <h1>CAR POOL BOOKING</h1>
             <div>calendar</div>
+            <Agenda/>
             <div>Enter driver name:</div>
-            <input type="text"/>
+            <input type="text" />
             <div>Enter destination</div>
-            <input type="text"/>
+            <input type="text" />
         </div>
     )
 }
