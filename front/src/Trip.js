4aec60a2c35ddf6a8a6f7a9b524980b46591f09f
Inês Oliveira <inesfpoliveira@gmail.com>
2020-02-06 17:01:31 +0000

    add the first part of the triplog css

--- front/src/Trip.js
+++ front/src/Trip.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import TripCard from './TripCard';
+import './triplog.css';
 
 const Trip = ({ data }) => {
     return(
         <>
-            <h1>Select your trip/Trips log</h1>
-            <select name="driver" id="driver">
-                <option value="driver">driver</option>
-            </select>
-            <Link to='/tripkm'>Trip Km</Link>
-            { data.map(trip => <TripCard trip={trip} />) }
+            <h1 class="triplog_title">Select your trip/Trips log</h1>
+            <div className="triplog_content">
+                <select name="driver" id="driver">
+                    <option value="driver">driver</option>
+                </select>
+                <Link to='/tripkm'>Trip Km</Link>
+                { data.map(trip => <TripCard trip={trip} />) }
+            </div>
         </>
     )
 }
--- front/src/TripCard.js
+++ front/src/TripCard.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Moment from 'react-moment';
 import Button from './Button';
+import './triplog.css';
 
 const TripCard = ({ trip }) => {
     console.log(trip);
     return(
-        <>
+        <div className="triplog_card">
             <div>Driver: {trip.driver_name}</div>
             <div>Reservation date: <Moment format='DD/MMM/YYYY'>{trip.book_date}</Moment></div>
             <div>Destination: {trip.destination}</div>
             <div>Start: {trip.start_time.slice(0, -3)}</div>
             <div>End: {trip.end_time.slice(0, -3)}</div>
             <Button />
-        </>
+        </div>
     )
 }
 
