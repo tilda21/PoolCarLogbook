d2d653c665734a997498074c3a38a79d21b8d36e
WildCodeSchool <anelisynagano@gmail.com>
2020-02-06 18:32:11 +0000

    a

--- front/src/Booking.js
+++ front/src/Booking.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import Agenda from './Agenda';
 
-const Booking = () => {
+const Booking = ({ data }) => {
     return (
         <div className='booking-container'>
             <h1>CAR POOL BOOKING</h1>
-            <div>calendar</div>
-            <Agenda/>
             <div>Enter driver name:</div>
             <input type="text" />
             <div>Enter destination</div>
             <input type="text" />
+            <Agenda data={data} />
         </div>
     )
 }
--- front/src/TripCard.js
+++ front/src/TripCard.js
@@ -3,14 +3,14 @@ import Moment from 'react-moment';
 import Button from './Button';
 
 const TripCard = ({ trip }) => {
-    console.log(trip);
+
     return(
         <>
             <div>Driver: {trip.driver_name}</div>
             <div>Reservation date: <Moment format='DD/MMM/YYYY'>{trip.book_date}</Moment></div>
             <div>Destination: {trip.destination}</div>
-            <div>Start: {trip.start_time.slice(0, -3)}</div>
-            <div>End: {trip.end_time.slice(0, -3)}</div>
+            <div>Start: {trip.book_date}</div>
+            <div>End: {trip.book_date}</div>
             <Button />
         </>
     )
