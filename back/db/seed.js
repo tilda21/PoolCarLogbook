ec7a8aab8b828b51797449fa7f6343508c75cf75
RaquelMarques97 <araquelfmarques@gmail.com>
2020-02-06 20:11:25 +0000

    little changes

--- back/db/seed.js
+++ back/db/seed.js
@@ -2,8 +2,9 @@ const connection = require('./config');
 
 const insertRecords = new Promise(
 	(resolve, reject) => connection.query(`INSERT INTO records (book_date, start_time, end_time, start_km, end_km, driver_name, car_plate, destination) VALUES 
-		('2020-02-10', '10:00:00', '11:00:00','56789', '56890', 'raquel', 'AD-35-GH', 'IKEA'),
-        ('2020-02-11', '10:00:00', '11:00:00','69090', '70000', 'matilda', 'AD-35-GH', 'porto')      
+		
+		('2020-02-11', '10:00:00', '11:00:00', null, null, 'raquel', 'AD-35-GH', 'porto'), 
+		('2020-02-11', '10:00:00', '11:00:00', null, null, 'matilda', 'AD-35-GH', 'porto') 
     `, (err) => {
 		if (err) reject(err);
 		console.log('records inserted');
@@ -11,3 +12,6 @@ const insertRecords = new Promise(
 		connection.end();
 	})
 );
+
+//('2020-02-10', '10:00:00', '11:00:00','56789', '56890', 'raquel', 'AD-35-GH', 'IKEA'),
+		//('2020-02-11', '10:00:00', '11:00:00','69090', '70000', 'matilda', 'AD-35-GH', 'porto'),
