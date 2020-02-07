0e41719a1f9154cc4cc123dc91a3a3f8c852f2ea
tilda21 <tilda_26@hotmail.com>
2020-02-07 15:50:02 +0000

    gulgulugl

--- front/src/Booking.js
+++ front/src/Booking.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React , { useState }  from 'react';
 import Agenda from './Agenda';
 import './booking.css';
 
 const Booking = ({ data }) => {
+    const [driver_name, setDriverName] = useState('');
+    const [destination, setDestination] = useState('');
+
+    let updateDataField = (e) => {
+        if(e.target.name=='driver_name'){
+            setDriverName(e.target.value);
+        }
+        else{
+            setDestination(e.target.value);
+        }
+    }
+
+    let handleSubmit = (e) => {
+        alert('Nice Try')
+    }
+
     return (
         <div className='booking-container'>
             <h1>CAR POOL BOOKING</h1>
-            <div>Enter driver name:</div>
-            <input type="text" />
-            <div>Enter destination</div>
-            <input type="text" />
+            <form onSubmit={handleSubmit} >
+                <label>
+                        Enter Driver Name:
+                    <input type="text" name="driver_name"  onChange={updateDataField} value={driver_name} />
+                </label>
+                
+                <label>
+                        Enter Destination:
+                    <input type="text" name="destination" onChange={updateDataField} value={destination}/>
+                </label>
+                <input type='submit' value='Submit' />
+            </form>
+            
+            
             <Agenda data={data} />
         </div>
     )
